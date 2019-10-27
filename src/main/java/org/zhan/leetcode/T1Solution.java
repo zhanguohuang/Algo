7d21fc2baed7b197778893f4983d2e2d17e517de
zhanguohuang <zhanguohuang@huarong.com>
2019-10-27 23:51:46 +0800

    first commit

---
+++ src/main/java/org/zhan/leetcode/T1Solution.java
@@ -0,0 +1,40 @@
+package org.zhan.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+ 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+
+ 给定 nums = [2, 7, 11, 15], target = 9
+
+ 因为 nums[0] + nums[1] = 2 + 7 = 9
+ 所以返回 [0, 1]
+
+ */
+public class T1Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0;i<nums.length;i++) {
+            map.put(nums[i], i);
+        }
+        for (int i=0;i<nums.length-1;i++) {
+            int del = target - nums[i];
+            if(map.keySet().contains(del) && map.get(del) != i) {
+                result = new int[]{i, map.get(target - nums[i])};
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[]{3, 2, 4}, 6);
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
